6b40a177d660903a52fd7773ddc6402b19e06fee
Erioro Paul <erioroegbelughepaul@gmail.com>
2023-10-21 02:35:45 +0100

    new

--- kiraniApp.js
+++ kiraniApp.js
@@ -1,4 +1,13 @@
-subMenu = document.getElementById("subMenu");
-             function toggleMenu(){
-                subMenu.classList.toggle("openMenu");
-             }
+const menuIcon = document.querySelector('.menuIcon');
+const subMenu = document.querySelector('.subMenuWrap');
+
+menuIcon.addEventListener('click', () => {
+    if (subMenu.style.display === 'block') {
+        subMenu.style.display = 'none';
+    } else {
+        subMenu.style.display = 'block';
+        subMenu.style.top = '50%';
+        subMenu.style.left = '50%';
+        subMenu.style.transform = 'translate(-50%, -50%)';
+    }
+});
